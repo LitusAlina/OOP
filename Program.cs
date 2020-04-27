0ed30225217a37f7db3a039407d6f8e2a40f349e
LitusAlina <56652716+LitusAlina@users.noreply.github.com>
2020-04-27 22:00:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,118 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace lab4cs
+namespace lab5SH2
 {
-    class Section
+    class String
     {
-        private int begX;
-        private int endX;
-        private int begY;
-        private int endY;
-        int x, y;
-        public Section()
+     public
+         int str_len() { return 0; }
+         string char_replace() { return ""; }
+    }
+
+    class Characters : String
+    {
+        private string s;
+
+        public Characters(string s1)
         {
-            begX = 0;
-            endX = 0;
-            x = 0;
-            begY = 0;
-            endY = 0;
-            y = 0;
-        }
-        public Section(int valuebegX, int valueendX, int valuebegY, int valueendY)
-        {
-            begX = valuebegX;
-            endX = valueendX;
-            begY = valuebegY;
-            endY = valueendY;
-            x = endX - begX;
-            y = endY - begY;
-        }
-        public int GetbegX()
-        {
-            return begX;
-        }
-        public int GetendX()
-        {
-            return endX;
-        }
-        public int GetbegY()
-        {
-            return begY;
-        }
-        public int GetendY()
-        {
-            return endY;
-        }
-        public int Getx()
-        {
-            return x;
-        }
-        public int Gety()
-        {
-            return y;
+            s = s1;
         }
 
-        public Section(Section other)
+       public int str_len(string s) 
         {
-            //конструктор копирования
-            this.x = other.x;
-            this.y = other.y;
+            int len = s.Length;
+           
+            Console.WriteLine(len);
+            return len;
         }
-        public static Section operator -(Section obj, Section other)
+                 
+        public string char_replace()
         {
-            Section temp1 = new Section();
-            temp1.x = obj.endX - other.begX;
-            temp1.y = obj.endY - other.begY;
-            return temp1;
-        }
-        public static Section operator +(Section obj, Section other)
-        {
-            Section temp2 = new Section();
-            temp2.x = obj.x + other.x;
-            temp2.y = obj.y + other.y;
-            return temp2;
-        }
-        public static Section operator *(Section obj, int a)
-        {
-            Section temp3 = new Section();
-            temp3.x = obj.x * a;
-            temp3.y = obj.y * a;
-            return temp3;
-        }
-        public double Length()
-        {
-            double result;
-            result = Math.Sqrt(this.x * this.x + this.y * this.y);
-            Console.WriteLine(result);
-            return result;
+            int counter = 0;
+            for (int i = 0; i < s.Length; i++) { if (s[i] == '#') counter++; }
+            char[] arr = new char[s.Length];
+            Array.Resize(ref arr, s.Length + counter);
+            for (int i = 0, j = 0; i < s.Length; i++, j++)
+            {
+                if (s[i] != '#') arr[j] = s[i];
+                else
+                {
+                    arr[j] = '!';
+                    arr[j + 1] = '!';
+                    j++;
+                }
+            }
+            s = new string(arr);
+            return s;
         }
         public void Print()
         {
-            Console.WriteLine("L\t" + x + "\t" + y);
-            Console.WriteLine("\n");
+            Console.WriteLine(s, str_len(s));
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Section L2 = new Section(3, 8, 4, 16);
-            Section L3 = new Section(1, 4, 5, 9);
-            Section L1 = L2 + L3;
-            L1.Print();
-            L2.Print();
-            L3.Print();
-            L1.Length();
-            L2.Length();
-            L3.Length();
 
-            Section L4 = L3 * 2;
-            L4.Print();
-            L4.Length();
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Characters c = new Characters("gvh23#$%#");
+                c.Print();
+                c.str_len();
+                c.char_replace();
+                c.str_len();
+                c.Print();
+                
+            }
         }
-    }
+    
 }
